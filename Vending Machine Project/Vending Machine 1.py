5920570727394a2191756f63701fb9649ada1315
Peter Ye <pye07@ucla.edu>
2022-09-26 11:56:58 -0700

    test connection

--- Vending Machine Project/Vending Machine 1.py
+++ Vending Machine Project/Vending Machine 1.py
@@ -1,8 +1,9 @@
 payment = float(input("Coffee costs $3.50. How much cash will you give? $"))
-if payment < float(3.5):
+price = float(3.50)
+if payment < price:
     print("Insufficient funds.")
-elif payment == float(3.5):
+elif payment == price:
     print ("Your drink will be out soon. Thank you!")
-elif payment > float(3.5):
+elif payment > price:
     change = float(payment - 3.5)
     print ("Your change is $", change, ". Your drink will be out soon. Thank you!")
